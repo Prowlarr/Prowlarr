93f4eae6c425ae9db929bb4235e125d54e7e84d0
Qstick <qstick@gmail.com>
2021-02-19 23:13:00 -0500

    New: Indexer - BrokenStones

---
+++ src/NzbDrone.Core/Indexers/Definitions/BrokenStones.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using NLog;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.Configuration;
+
+namespace NzbDrone.Core.Indexers.Definitions
+{
+    public class BrokenStones : Gazelle.Gazelle
+    {
+        public override string Name => "BrokenStones";
+        public override string BaseUrl => "https://brokenstones.club/";
+        public override IndexerPrivacy Privacy => IndexerPrivacy.Private;
+
+        public BrokenStones(IHttpClient httpClient, IIndexerStatusService indexerStatusService, IConfigService configService, Logger logger)
+            : base(httpClient, indexerStatusService, configService, logger)
+        {
+        }
+
+        protected override IndexerCapabilities SetCapabilities()
+        {
+            var caps = new IndexerCapabilities
+            {
+            };
+
+            caps.Categories.AddCategoryMapping(1, NewznabStandardCategory.PCMac, "MacOS Apps");
+            caps.Categories.AddCategoryMapping(2, NewznabStandardCategory.PCMac, "MacOS Games");
+            caps.Categories.AddCategoryMapping(3, NewznabStandardCategory.PCMobileiOS, "iOS Apps");
+            caps.Categories.AddCategoryMapping(4, NewznabStandardCategory.PCMobileiOS, "iOS Games");
+            caps.Categories.AddCategoryMapping(5, NewznabStandardCategory.Other, "Graphics");
+            caps.Categories.AddCategoryMapping(6, NewznabStandardCategory.Audio, "Audio");
+            caps.Categories.AddCategoryMapping(7, NewznabStandardCategory.Other, "Tutorials");
+            caps.Categories.AddCategoryMapping(8, NewznabStandardCategory.Other, "Other");
+
+            return caps;
+        }
+    }
+}

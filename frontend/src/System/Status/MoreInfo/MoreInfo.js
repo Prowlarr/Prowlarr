8fe8e55685e8e6634678702d10c564601525a494
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-02-21 11:08:46 -0600

    Fixed: Updates to System > More Info

--- frontend/src/System/Status/MoreInfo/MoreInfo.js
+++ frontend/src/System/Status/MoreInfo/MoreInfo.js
@@ -15,32 +15,32 @@ class MoreInfo extends Component {
     return (
       <FieldSet legend={translate('MoreInfo')}>
         <DescriptionList>
-          <DescriptionListItemTitle>Home page</DescriptionListItemTitle>
+          <DescriptionListItemTitle>{translate('HomePage')}</DescriptionListItemTitle>
           <DescriptionListItemDescription>
             <Link to="https://prowlarr.com/">prowlarr.com</Link>
           </DescriptionListItemDescription>
 
-          <DescriptionListItemTitle>Discord</DescriptionListItemTitle>
-          <DescriptionListItemDescription>
-            <Link to="https://prowlarr.com/discord">prowlarr.com/discord</Link>
-          </DescriptionListItemDescription>
-
-          <DescriptionListItemTitle>Wiki</DescriptionListItemTitle>
+          <DescriptionListItemTitle>{translate('Wiki')}</DescriptionListItemTitle>
           <DescriptionListItemDescription>
             <Link to="https://wikijs.servarr.com/prowlarr">wikijs.servarr.com/prowlarr</Link>
           </DescriptionListItemDescription>
 
-          <DescriptionListItemTitle>Donations</DescriptionListItemTitle>
+          <DescriptionListItemTitle>{translate('Reddit')}</DescriptionListItemTitle>
           <DescriptionListItemDescription>
-            <Link to="https://opencollective.com/prowlarr">opencollective.com/prowlarr</Link>
+            <Link to="https://reddit.com/r/prowlarr">r/prowlarr</Link>
           </DescriptionListItemDescription>
 
-          <DescriptionListItemTitle>Source</DescriptionListItemTitle>
+          <DescriptionListItemTitle>{translate('Discord')}</DescriptionListItemTitle>
+          <DescriptionListItemDescription>
+            <Link to="https://prowlarr.com/discord">prowlarr.com/discord</Link>
+          </DescriptionListItemDescription>
+
+          <DescriptionListItemTitle>{translate('Source')}</DescriptionListItemTitle>
           <DescriptionListItemDescription>
             <Link to="https://github.com/Prowlarr/Prowlarr/">github.com/Prowlarr/Prowlarr</Link>
           </DescriptionListItemDescription>
 
-          <DescriptionListItemTitle>Feature Requests</DescriptionListItemTitle>
+          <DescriptionListItemTitle>{translate('FeatureRequests')}</DescriptionListItemTitle>
           <DescriptionListItemDescription>
             <Link to="https://github.com/Prowlarr/Prowlarr/issues">github.com/Prowlarr/Prowlarr/issues</Link>
           </DescriptionListItemDescription>

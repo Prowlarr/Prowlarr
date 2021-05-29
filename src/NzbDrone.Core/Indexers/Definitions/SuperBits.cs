6ae925467840f2f0d1864fb24d6029c0f235315a
Qstick <qstick@gmail.com>
2021-05-29 16:10:02 -0400

    Fixed: (SuperBits) Use Cookie for grabs

--- src/NzbDrone.Core/Indexers/Definitions/SuperBits.cs
+++ src/NzbDrone.Core/Indexers/Definitions/SuperBits.cs
@@ -43,6 +43,11 @@ namespace NzbDrone.Core.Indexers.Definitions
             return new SuperBitsParser(Settings, Capabilities.Categories, BaseUrl);
         }
 
+        protected override IDictionary<string, string> GetCookies()
+        {
+            return CookieUtil.CookieHeaderToDictionary(Settings.Cookie);
+        }
+
         private IndexerCapabilities SetCapabilities()
         {
             var caps = new IndexerCapabilities
@@ -147,11 +152,6 @@ namespace NzbDrone.Core.Indexers.Definitions
             var request = new IndexerRequest(searchUrl, HttpAccept.Json);
 
             request.HttpRequest.Headers.Add("Referer", BaseUrl);
-
-            foreach (var cookie in CookieUtil.CookieHeaderToDictionary(Settings.Cookie))
-            {
-                request.HttpRequest.Cookies.Add(cookie.Key, cookie.Value);
-            }
 
             yield return request;
         }

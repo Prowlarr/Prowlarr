593231c3af38f36153fb4ca23719d21a97c995c8
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2021-06-04 23:07:40 +0100

    Updated config definition to match Jacketts config definition

--- src/NzbDrone.Core/Migration/JackettIndexerConfigDefintion.cs
+++ src/NzbDrone.Core/Migration/JackettIndexerConfigDefintion.cs
@@ -8,10 +8,16 @@ namespace NzbDrone.Core.Migration
 {
     public class JackettIndexerConfigDefintion
     {
+        public string id { get; set; }
+        public string type { get; set; } //DO NOT THINK THIS IS REQUIRED
+        public string name { get; set; } //Again doubt it's needed
+        public string value { get; set; }
+
+        //BELOW ARE THE DIFFERENT THINGS FROM NIT
         //TODO: Update to ensure json matches up with Prowlarrs
         public string Cookie { get; set; }
         public string Username { get; set; }
-        public string MFA { get; set; }
+        public string 2FA { get; set; }
         public string Email { get; set; }
         public string Pid { get; set; }
         public string Pin { get; set;  }

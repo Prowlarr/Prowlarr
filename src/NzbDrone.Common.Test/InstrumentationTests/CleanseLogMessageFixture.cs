247db87dd9619bddd69c5ba17dcb84398228dd29
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-06-09 20:53:48 -0500

    Fixed: Cleanse AuthKey followed by Double Quote

--- src/NzbDrone.Common.Test/InstrumentationTests/CleanseLogMessageFixture.cs
+++ src/NzbDrone.Common.Test/InstrumentationTests/CleanseLogMessageFixture.cs
@@ -19,6 +19,7 @@ namespace NzbDrone.Common.Test.InstrumentationTests
         [TestCase(@"http://nzb.su/getnzb/2b51db35e1912ffc138825a12b9933d2.nzb&i=37292&r=2b51db35e1910123321025a12b9933d2")]
         [TestCase(@"https://horrorcharnel.org/takeloginhorror.php: username=mySecret&password=mySecret&use_sslvalue==&perm_ssl=1&submitme=X&use_ssl=1&returnto=%2F&captchaSelection=1230456")]
         [TestCase(@"https://torrentdb.net/login: _token=2b51db35e1912ffc138825a12b9933d2&username=mySecret&password=mySecret&remember=on")]
+        [TestCase(@"feeds.php?feed=torrents_lossless24&amp;user=mySecret&amp;auth=mySecret&amp;passkey=mySecret&amp;authkey=2b51db35e1910123321025a12b9933d2""")]
 
         // NzbGet
         [TestCase(@"{ ""Name"" : ""ControlUsername"", ""Value"" : ""mySecret"" }, { ""Name"" : ""ControlPassword"", ""Value"" : ""mySecret"" }, ")]

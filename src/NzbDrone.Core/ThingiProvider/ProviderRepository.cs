19e9d55db2d347d28422f4458ad91cae80aece37
Yukine <devyukine@gmx.de>
2021-06-15 15:38:52 +0200

    Revert "fix(ProviderRepository): remove DictionaryKeyPolicy CamelCase Naming"
    This reverts commit 90c95b240f5f51c7280aa5f314a52f72d5b92d89.

--- src/NzbDrone.Core/ThingiProvider/ProviderRepository.cs
+++ src/NzbDrone.Core/ThingiProvider/ProviderRepository.cs
@@ -23,6 +23,7 @@ namespace NzbDrone.Core.ThingiProvider
                 AllowTrailingCommas = true,
                 IgnoreNullValues = true,
                 PropertyNameCaseInsensitive = true,
+                DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 WriteIndented = true
             };

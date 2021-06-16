eeec505182f879fde8d9e28cde18a7717fcd204f
Yukine <DevYukine@gmx.de>
2021-06-16 03:13:37 +0200

    Fixed: Remove Camel Case DB Converter for Cookies (#223)
    * fix(ProviderRepository): remove DictionaryKeyPolicy CamelCase Naming
    
    * Revert "fix(ProviderRepository): remove DictionaryKeyPolicy CamelCase Naming"
    
    This reverts commit 90c95b240f5f51c7280aa5f314a52f72d5b92d89.
    
    * fix(TableMapping): make own non Camel Case Converter for Cookies

---
+++ src/NzbDrone.Core/Datastore/Converters/CookieConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Text.Json;
+using Dapper;
+using NzbDrone.Common.Serializer;
+
+namespace NzbDrone.Core.Datastore.Converters
+{
+    public class CookieConverter : SqlMapper.TypeHandler<IDictionary<string, string>>
+    {
+        protected readonly JsonSerializerOptions SerializerSettings;
+
+        public CookieConverter()
+        {
+            var serializerSettings = new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+                IgnoreNullValues = true,
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+
+            SerializerSettings = serializerSettings;
+        }
+
+        public override void SetValue(IDbDataParameter parameter, IDictionary<string, string> value)
+        {
+            parameter.Value = JsonSerializer.Serialize(value, SerializerSettings);
+        }
+
+        public override IDictionary<string, string> Parse(object value)
+        {
+            return JsonSerializer.Deserialize<Dictionary<string, string>>((string)value, SerializerSettings);
+        }
+    }
+}

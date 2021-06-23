b6238f469cfd3f84e4b7185a8351631e6751de91
Qstick <qstick@gmail.com>
2021-06-23 07:37:37 -0400

    Fixed: (Orpheus) Use default Gazelle download links

--- src/NzbDrone.Core/Indexers/Definitions/Orpheus.cs
+++ src/NzbDrone.Core/Indexers/Definitions/Orpheus.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using NLog;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Configuration;
-using NzbDrone.Core.Indexers.Gazelle;
 using NzbDrone.Core.Messaging.Events;
 
 namespace NzbDrone.Core.Indexers.Definitions
@@ -49,30 +48,6 @@ namespace NzbDrone.Core.Indexers.Definitions
             caps.Categories.AddCategoryMapping(7, NewznabStandardCategory.Books, "Comics");
 
             return caps;
-        }
-
-        public override IParseIndexerResponse GetParser()
-        {
-            return new OrpheusParser(Settings, Capabilities, BaseUrl);
-        }
-    }
-
-    public class OrpheusParser : GazelleParser
-    {
-        public OrpheusParser(GazelleSettings settings, IndexerCapabilities capabilities, string baseUrl)
-            : base(settings, capabilities, baseUrl)
-        {
-        }
-
-        protected override string GetDownloadUrl(int torrentId)
-        {
-            var url = new HttpUri(_baseUrl)
-                .CombinePath("/ajax.php")
-                .AddQueryParam("action", "download")
-                .AddQueryParam("useToken", _settings.UseFreeleechToken ? "1" : "0")
-                .AddQueryParam("id", torrentId);
-
-            return url.FullUri;
         }
     }
  }

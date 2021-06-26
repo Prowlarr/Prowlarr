18213cfaf64e92d3451baa7768af3cd4376a6646
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-06-26 06:00:37 -0500

    Fixed: Default Branch is now Develop

--- src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
+++ src/NzbDrone.Core/Configuration/ConfigFileProvider.cs
@@ -180,7 +180,7 @@ namespace NzbDrone.Core.Configuration
         public bool AnalyticsEnabled => GetValueBoolean("AnalyticsEnabled", true, persist: false);
 
         // TODO: Change back to "master" for the first stable release.
-        public string Branch => GetValue("Branch", "nightly").ToLowerInvariant();
+        public string Branch => GetValue("Branch", "develop").ToLowerInvariant();
 
         public string LogLevel => GetValue("LogLevel", "info").ToLowerInvariant();
         public string ConsoleLogLevel => GetValue("ConsoleLogLevel", string.Empty, persist: false);

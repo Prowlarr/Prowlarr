5cffb10e08d27e7cf73edb277ee071531e6ddf04
Lagicrus <Lagicrus@users.noreply.github.com>
2021-07-05 13:42:51 +0100

    Fixed: If no categories are passed in, flag up a unknown error (#311)
    * If no categories are passed in, flag up a unknown error
    
    * Pass back in default props to deal with undefined issues

--- frontend/src/Search/Table/CategoryLabel.js
+++ frontend/src/Search/Table/CategoryLabel.js
@@ -1,9 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Label from 'Components/Label';
+import { kinds, tooltipPositions } from 'Helpers/Props';
+import Tooltip from '../../Components/Tooltip/Tooltip';
 
-function CategoryLabel({ categories = [] }) {
+function CategoryLabel({ categories }) {
   const sortedCategories = categories.filter((cat) => cat.name !== undefined).sort((c) => c.id);
+
+  if (categories?.length === 0) {
+    return (
+      <Tooltip
+        anchor={<Label kind={kinds.DANGER}>Unknown</Label>}
+        tooltip="Please report this issue to the GitHub as this shouldn't be happening"
+        position={tooltipPositions.LEFT}
+      />
+    );
+  }
 
   return (
     <span>
@@ -20,8 +32,12 @@ function CategoryLabel({ categories = [] }) {
   );
 }
 
+CategoryLabel.defaultProps = {
+  categories: []
+};
+
 CategoryLabel.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.object)
+  categories: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default CategoryLabel;

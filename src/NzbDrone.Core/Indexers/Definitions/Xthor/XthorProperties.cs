2b535a5bde81a78f7cb7f663ae09842fbc4c3e3e
Thomas <thomasclem33@gmail.com>
2021-07-14 12:10:09 +0200

    New: (Indexer) Torrent Xthor - Add "EnhancedFrenchAccent" field that will allow to find VF2 releases when searching VFF or VFQ - Fix BaseSettings - Decrease the RateLimit to 2.1 seconds - Remove page argument when searching page 0

--- src/NzbDrone.Core/Indexers/Definitions/Xthor/XthorProperties.cs
+++ src/NzbDrone.Core/Indexers/Definitions/Xthor/XthorProperties.cs
@@ -15,7 +15,7 @@ namespace NzbDrone.Core.Indexers.Definitions.Xthor
         VFQ = 2,
 
         [FieldOption(Hint = "Françaises et Québécoises")]
-        VF = 47,
+        VF2 = 47,
 
         [FieldOption(Hint = "Anglaises")]
         EN = 3,
@@ -33,7 +33,121 @@ namespace NzbDrone.Core.Indexers.Definitions.Xthor
         CH = 7,
 
         [FieldOption(Hint = "Italiennes")]
-        IT = 8
+        IT = 8,
+
+        [FieldOption(Hint = "Coréennes")]
+        KR = 9,
+
+        [FieldOption(Hint = "Danoises")]
+        DK = 10,
+
+        [FieldOption(Hint = "Russes")]
+        RU = 11,
+
+        [FieldOption(Hint = "Portugaises")]
+        PT = 12,
+
+        [FieldOption(Hint = "Hindi")]
+        IN = 13,
+
+        [FieldOption(Hint = "Hollandaises")]
+        NL = 14,
+
+        [FieldOption(Hint = "Suédoises")]
+        SE = 15,
+
+        [FieldOption(Hint = "Norvégiennes")]
+        NO = 16,
+
+        [FieldOption(Hint = "Thaïlandaises")]
+        TH = 17,
+
+        [FieldOption(Hint = "Hébreu")]
+        HE = 18,
+
+        [FieldOption(Hint = "Persanes")]
+        PE = 19,
+
+        [FieldOption(Hint = "Arabes")]
+        AR = 20,
+
+        [FieldOption(Hint = "Turques")]
+        TR = 21,
+
+        [FieldOption(Hint = "Hongroises")]
+        HU = 22,
+
+        [FieldOption(Hint = "Polonaises")]
+        PL = 23,
+
+        [FieldOption(Hint = "Finnoises")]
+        FI = 24,
+
+        [FieldOption(Hint = "Indonésiennes")]
+        ID = 25,
+
+        [FieldOption(Hint = "Roumaines")]
+        RO = 26,
+
+        [FieldOption(Hint = "Malaisiennes")]
+        MY = 27,
+
+        [FieldOption(Hint = "Estoniennes")]
+        EE = 28,
+
+        [FieldOption(Hint = "Islandaises")]
+        IS = 29,
+
+        [FieldOption(Hint = "Grecques")]
+        GR = 30,
+
+        [FieldOption(Hint = "Serbes")]
+        RS = 31,
+
+        [FieldOption(Hint = "Norvégiennes (2)")]
+        NOB = 32,
+
+        [FieldOption(Hint = "Ukrainiennes")]
+        UA = 33,
+
+        [FieldOption(Hint = "Bulgares")]
+        BG = 34,
+
+        [FieldOption(Hint = "Tagalogues")]
+        PH = 35,
+
+        [FieldOption(Hint = "Xhosa")]
+        XH = 36,
+
+        [FieldOption(Hint = "Kurdes")]
+        KU = 37,
+
+        [FieldOption(Hint = "Bengali")]
+        BE = 38,
+
+        [FieldOption(Hint = "Amhariques")]
+        AM = 39,
+
+        [FieldOption(Hint = "Bosniaques")]
+        BO = 40,
+
+        [FieldOption(Hint = "Malayalam")]
+        MA = 41,
+
+        [FieldOption(Hint = "Télougou")]
+        TE = 42,
+
+        [FieldOption(Hint = "Bambara")]
+        BA = 43,
+
+        [FieldOption(Hint = "Catalanes")]
+        CT = 44,
+
+        [FieldOption(Hint = "Tchèques")]
+        CZ = 45,
+
+        [FieldOption(Hint = "Afrikaans")]
+        AF = 46,
     }
 
     public enum XthorPagesNumber

f46f65b0f577362a566cf8bc540d3c47a15e677d
chryzsh <>
2021-08-02 11:02:09 +0200

    fixed imdb search for sc

--- src/NzbDrone.Core/Indexers/Definitions/SecretCinema.cs
+++ src/NzbDrone.Core/Indexers/Definitions/SecretCinema.cs
@@ -26,6 +26,9 @@ namespace NzbDrone.Core.Indexers.Definitions
     {
         public override string Name => "Secret Cinema";
         public override string[] IndexerUrls => new string[] { "https://secret-cinema.pw/" };
+        public bool ImdbInTags => false;
+        public string APIUrl => Settings.BaseUrl + "ajax.php";
+        public string GetSearchTerm(string term) => term;
         public override string Description => "A tracker for rare movies.";
         public override DownloadProtocol Protocol => DownloadProtocol.Torrent;
         public override IndexerPrivacy Privacy => IndexerPrivacy.Private;
@@ -36,9 +39,51 @@ namespace NzbDrone.Core.Indexers.Definitions
         {
         }
 
+        public override IIndexerRequestGenerator GetRequestGenerator()
+        {
+            return new SecretCinemaRequestGenerator() { Settings = Settings, Capabilities = Capabilities, HttpClient = _httpClient };
+        }
+
         public override IParseIndexerResponse GetParser()
         {
             return new SecretCinemaParser(Settings, Capabilities);
+        }
+
+        private IEnumerable<IndexerRequest> GetRequest(string searchParameters)
+        {
+            var filter = "";
+            if (searchParameters == null)
+            {
+            }
+
+            var request =
+                new IndexerRequest(
+                    $"{APIUrl}?{searchParameters}{filter}",
+                    HttpAccept.Json);
+
+            yield return request;
+        }
+
+        private string GetBasicSearchParameters(string searchTerm, int[] categories)
+        {
+            var searchString = GetSearchTerm(searchTerm);
+
+            var parameters = "action=browse&order_by=time&order_way=desc";
+
+            if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                parameters += string.Format("&searchstr={0}", searchString);
+            }
+
+            if (categories != null)
+            {
+                foreach (var cat in Capabilities.Categories.MapTorznabCapsToTrackers(categories))
+                {
+                    parameters += string.Format("&filter_cat[{0}]=1", cat);
+                }
+            }
+
+            return parameters;
         }
 
         protected override IndexerCapabilities SetCapabilities()
@@ -47,7 +92,7 @@ namespace NzbDrone.Core.Indexers.Definitions
             {
                 MovieSearchParams = new List<MovieSearchParam>
                        {
-                           MovieSearchParam.Q
+                           MovieSearchParam.Q, MovieSearchParam.ImdbId
                        },
                 MusicSearchParams = new List<MusicSearchParam>
                        {
@@ -60,8 +105,159 @@ namespace NzbDrone.Core.Indexers.Definitions
 
             return caps;
         }
+    }
 
-        public class SecretCinemaParser : IParseIndexerResponse
+    public class SecretCinemaRequestGenerator : IIndexerRequestGenerator
+    {
+        public GazelleSettings Settings { get; set; }
+
+        public IDictionary<string, string> AuthCookieCache { get; set; }
+        public IHttpClient HttpClient { get; set; }
+        public IndexerCapabilities Capabilities { get; set; }
+        public Logger Logger { get; set; }
+
+        protected virtual string APIUrl => Settings.BaseUrl + "ajax.php";
+        protected virtual bool ImdbInTags => false;
+
+        public Func<IDictionary<string, string>> GetCookies { get; set; }
+        public Action<IDictionary<string, string>, DateTime?> CookiesUpdater { get; set; }
+
+        public virtual IndexerPageableRequestChain GetRecentRequests()
+        {
+            var pageableRequests = new IndexerPageableRequestChain();
+
+            pageableRequests.Add(GetRequest(null));
+
+            return pageableRequests;
+        }
+
+        private IEnumerable<IndexerRequest> GetRequest(string searchParameters)
+        {
+            var filter = "";
+            if (searchParameters == null)
+            {
+            }
+
+            var request =
+                new IndexerRequest(
+                    $"{APIUrl}?{searchParameters}{filter}",
+                    HttpAccept.Json);
+
+            yield return request;
+        }
+
+        private string GetBasicSearchParameters(string searchTerm, int[] categories)
+        {
+            var searchString = GetSearchTerm(searchTerm);
+
+            var parameters = "action=browse&order_by=time&order_way=desc";
+
+            if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                parameters += string.Format("&searchstr={0}", searchString);
+            }
+
+            if (categories != null)
+            {
+                foreach (var cat in Capabilities.Categories.MapTorznabCapsToTrackers(categories))
+                {
+                    parameters += string.Format("&filter_cat[{0}]=1", cat);
+                }
+            }
+
+            return parameters;
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(MovieSearchCriteria searchCriteria)
+        {
+            var parameters = GetBasicSearchParameters(searchCriteria.SearchTerm, searchCriteria.Categories);
+
+            if (searchCriteria.ImdbId != null)
+            {
+                if (ImdbInTags)
+                {
+                    parameters += string.Format("&taglist={0}", searchCriteria.ImdbId);
+                }
+                else
+                {
+                    // secret cinema uses the tt<id> format for imdb ids, so add that back in
+                    parameters += string.Format("&cataloguenumber=tt{0}", searchCriteria.ImdbId);
+                }
+            }
+
+            var pageableRequests = new IndexerPageableRequestChain();
+            pageableRequests.Add(GetRequest(parameters));
+            return pageableRequests;
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(MusicSearchCriteria searchCriteria)
+        {
+            var parameters = GetBasicSearchParameters(searchCriteria.SearchTerm, searchCriteria.Categories);
+
+            if (searchCriteria.Artist != null)
+            {
+                parameters += string.Format("&artistname={0}", searchCriteria.Artist);
+            }
+
+            if (searchCriteria.Label != null)
+            {
+                parameters += string.Format("&recordlabel={0}", searchCriteria.Label);
+            }
+
+            if (searchCriteria.Album != null)
+            {
+                parameters += string.Format("&groupname={0}", searchCriteria.Album);
+            }
+
+            var pageableRequests = new IndexerPageableRequestChain();
+            pageableRequests.Add(GetRequest(parameters));
+            return pageableRequests;
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(TvSearchCriteria searchCriteria)
+        {
+            var parameters = GetBasicSearchParameters(searchCriteria.SanitizedTvSearchString, searchCriteria.Categories);
+
+            if (searchCriteria.ImdbId != null)
+            {
+                if (ImdbInTags)
+                {
+                    parameters += string.Format("&taglist={0}", searchCriteria.ImdbId);
+                }
+                else
+                {
+                    parameters += string.Format("&cataloguenumber={0}", searchCriteria.ImdbId);
+                }
+            }
+
+            var pageableRequests = new IndexerPageableRequestChain();
+            pageableRequests.Add(GetRequest(parameters));
+            return pageableRequests;
+        }
+
+        public IndexerPageableRequestChain GetSearchRequests(BookSearchCriteria searchCriteria)
+        {
+            var parameters = GetBasicSearchParameters(searchCriteria.SearchTerm, searchCriteria.Categories);
+
+            var pageableRequests = new IndexerPageableRequestChain();
+            pageableRequests.Add(GetRequest(parameters));
+            return pageableRequests;
+        }
+
+        // hook to adjust the search term
+        protected virtual string GetSearchTerm(string term) => term;
+
+        public IndexerPageableRequestChain GetSearchRequests(BasicSearchCriteria searchCriteria)
+        {
+            var parameters = GetBasicSearchParameters(searchCriteria.SearchTerm, searchCriteria.Categories);
+
+            var pageableRequests = new IndexerPageableRequestChain();
+            pageableRequests.Add(GetRequest(parameters));
+            return pageableRequests;
+        }
+    }
+
+    public class SecretCinemaParser : IParseIndexerResponse
         {
             protected readonly GazelleSettings _settings;
             protected readonly IndexerCapabilities _capabilities;
@@ -233,5 +429,4 @@ namespace NzbDrone.Core.Indexers.Definitions
                 return url.FullUri;
             }
         }
-    }
 }

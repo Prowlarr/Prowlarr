19471bdd9b4251b882c7f1bb7ccdad94769d4669
philborman <phil.borman@gmail.com>
2021-08-23 11:23:37 +0200

    Changes as per Qstick request

---
+++ src/NzbDrone.Core/Applications/LazyLibrarianStatus.cs
@@ -0,0 +1,8 @@
+namespace NzbDrone.Core.Applications.LazyLibrarian
+{
+    public class LazyLibrarianStatus
+    {
+        public bool Success { get; set; }
+        public LazyLibrarianError Error { get; set; }
+    }
+}

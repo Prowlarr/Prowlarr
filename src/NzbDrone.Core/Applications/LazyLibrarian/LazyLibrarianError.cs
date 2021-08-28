98d2c63908c7632e86517d931860fa2acb0cc41e
Phil Borman <phil.borman@gmail.com>
2021-08-28 20:21:31 +0200

    Code tidying

---
+++ src/NzbDrone.Core/Applications/LazyLibrarian/LazyLibrarianError.cs
@@ -0,0 +1,8 @@
+namespace NzbDrone.Core.Applications.LazyLibrarian
+{
+    public class LazyLibrarianError
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}

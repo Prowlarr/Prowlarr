47e9c18985367a93ea8d332c46860db38b60fadf
Phil Borman <phil.borman@gmail.com>
2021-08-28 19:30:47 +0200

    Code cleanup

--- src/NzbDrone.Core/Applications/LazyLibrarianError.cs
+++
@@ -1,8 +0,0 @@
-namespace NzbDrone.Core.Applications.LazyLibrarian
-{
-    public class LazyLibrarianError
-    {
-        public int Code { get; set; }
-        public string Message { get; set; }
-    }
-}
--- src/NzbDrone.Core/Applications/LazyLibrarianStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace NzbDrone.Core.Applications.LazyLibrarian
-{
-    public class LazyLibrarianStatus
-    {
-        public bool Success { get; set; }
-        public LazyLibrarianError Error { get; set; }
-    }
-}

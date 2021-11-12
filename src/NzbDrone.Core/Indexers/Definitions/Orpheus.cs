bf428e84946bb382b207c7137db2202b3abc298a
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-11-12 13:38:23 -0600

    Fixed: Orpheus failing to grab if token is not used
    Closes #592

--- src/NzbDrone.Core/Indexers/Definitions/Orpheus.cs
+++ src/NzbDrone.Core/Indexers/Definitions/Orpheus.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using NLog;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Indexers.Gazelle;
 using NzbDrone.Core.Messaging.Events;
 
 namespace NzbDrone.Core.Indexers.Definitions
@@ -49,6 +50,35 @@ namespace NzbDrone.Core.Indexers.Definitions
             caps.Categories.AddCategoryMapping(7, NewznabStandardCategory.Books, "Comics");
 
             return caps;
+        }
+
+        public override IParseIndexerResponse GetParser()
+        {
+            return new OrpheusParser(Settings, Capabilities);
+        }
+    }
+
+    public class OrpheusParser : GazelleParser
+    {
+        public OrpheusParser(GazelleSettings settings, IndexerCapabilities capabilities)
+            : base(settings, capabilities)
+        {
+        }
+
+        protected override string GetDownloadUrl(int torrentId)
+        {
+            var url = new HttpUri(_settings.BaseUrl)
+                .CombinePath("/torrents.php")
+                .AddQueryParam("action", "download")
+                .AddQueryParam("id", torrentId);
+
+            // Orpheus fails to download if usetoken=0 so we need to only add if we will use one
+            if (_settings.UseFreeleechToken)
+            {
+                url.AddQueryParam("useToken", "1");
+            }
+
+            return url.FullUri;
         }
     }
 }

ce901ae8a36aac05ce181087e2b1abae264274ff
Qstick <qstick@gmail.com>
2021-11-21 17:46:24 -0600

    Update translate.js to use createAjaxRequest

--- frontend/src/Utilities/String/translate.js
+++ frontend/src/Utilities/String/translate.js
@@ -1,22 +1,18 @@
-import $ from 'jquery';
+import createAjaxRequest from 'Utilities/createAjaxRequest';
 
 function getTranslations() {
   let localization = null;
   const ajaxOptions = {
     async: false,
-    type: 'GET',
-    global: false,
     dataType: 'json',
-    url: `${window.Prowlarr.apiRoot}/localization`,
+    url: '/localization',
     success: function(data) {
       localization = data.Strings;
     }
   };
 
-  ajaxOptions.headers = ajaxOptions.headers || {};
-  ajaxOptions.headers['X-Api-Key'] = window.Prowlarr.apiKey;
+  createAjaxRequest(ajaxOptions);
 
-  $.ajax(ajaxOptions);
   return localization;
 }
 

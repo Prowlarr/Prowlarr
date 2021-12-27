177084fe8b7f5e413c752cf16f6e08c048ab4bdd
Gabriel Sjöberg <ga22be@live.se>
2021-12-27 23:20:49 +0100

    Fixed: (Indexer) Update RARBG API query options
    * Added app_id to captcha check to avoid 403 forbidden error
    * Migrated app_id from hard coded to BuildInfo.AppName

--- src/NzbDrone.Core/Indexers/Definitions/Rarbg/Rarbg.cs
+++ src/NzbDrone.Core/Indexers/Definitions/Rarbg/Rarbg.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NLog;
+using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Core.Configuration;
@@ -103,7 +104,7 @@ namespace NzbDrone.Core.Indexers.Rarbg
                 try
                 {
                     var request = new HttpRequestBuilder(Settings.BaseUrl.Trim('/'))
-                           .Resource("/pubapi_v2.php?get_token=get_token")
+                           .Resource($"/pubapi_v2.php?get_token=get_token&app_id={BuildInfo.AppName}")
                            .Accept(HttpAccept.Json)
                            .Build();
 

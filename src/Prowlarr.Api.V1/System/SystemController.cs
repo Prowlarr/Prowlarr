c9951e7eba3a70d1a9203410f6fcdeaf18dcb883
Qstick <qstick@gmail.com>
2022-01-18 23:29:30 -0600

    New: Add AppName to system status response

--- src/Prowlarr.Api.V1/System/SystemController.cs
+++ src/Prowlarr.Api.V1/System/SystemController.cs
@@ -58,6 +58,7 @@ namespace Prowlarr.Api.V1.System
         {
             return new
             {
+                AppName = BuildInfo.AppName,
                 Version = BuildInfo.Version.ToString(),
                 BuildTime = BuildInfo.BuildDateTime,
                 IsDebug = BuildInfo.IsDebug,

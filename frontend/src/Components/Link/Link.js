2f5b55013af063fd7ac659c692eab3b3db3734d5
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-03-08 16:09:52 -0600

    Fixed: '/indexers' URL Base breaking UI navigation
    (cherry picked from commit 140547e42a32cbaf80e7a794c5be9bfb574e97a2)

--- frontend/src/Components/Link/Link.js
+++ frontend/src/Components/Link/Link.js
@@ -47,10 +47,6 @@ class Link extends Component {
         el = 'a';
         linkProps.href = to;
         linkProps.target = target || '_self';
-      } else if (to.startsWith(`${window.Prowlarr.urlBase}/`)) {
-        el = RouterLink;
-        linkProps.to = to;
-        linkProps.target = target;
       } else {
         el = RouterLink;
         linkProps.to = `${window.Prowlarr.urlBase}/${to.replace(/^\//, '')}`;

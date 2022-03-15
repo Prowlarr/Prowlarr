55d03967ec70c59cc16ee199886dfb3886d9068c
Qstick <qstick@gmail.com>
2022-03-14 22:33:08 -0500

    Bump Swashbuckle to 6.3.0

--- docs.sh
+++ docs.sh
@@ -27,11 +27,11 @@ dotnet clean $slnFile -c Release
 dotnet msbuild -restore $slnFile -p:Configuration=Debug -p:Platform=$platform -p:RuntimeIdentifiers=$RUNTIME -t:PublishAllRids
 
 dotnet new tool-manifest
-dotnet tool install --version 6.2.3 Swashbuckle.AspNetCore.Cli
+dotnet tool install --version 6.3.0 Swashbuckle.AspNetCore.Cli
 
 dotnet tool run swagger tofile --output ./src/Prowlarr.Api.V1/openapi.json "$outputFolder/net6.0/$RUNTIME/prowlarr.console.dll" v1 &
 
-sleep 10
+sleep 30
 
 kill %1
 

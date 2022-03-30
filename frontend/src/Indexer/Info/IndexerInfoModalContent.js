2119a54e44f8634cb897b34a711dd002dc78cec3
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-03-30 07:43:03 -0500

    fixup! Fixed: Indexer Infobox Error

--- frontend/src/Indexer/Info/IndexerInfoModalContent.js
+++ frontend/src/Indexer/Info/IndexerInfoModalContent.js
@@ -77,7 +77,7 @@ function IndexerInfoModalContent(props) {
               <DescriptionListItem
                 descriptionClassName={styles.description}
                 title={translate('SearchTypes')}
-                data={capabilities.searchParams.length === 0 ? translate('NotSupported') : capabilities.search[0]}
+                data={capabilities.searchParams.length === 0 ? translate('NotSupported') : capabilities.searchParams[0]}
               />
               <DescriptionListItem
                 descriptionClassName={styles.description}

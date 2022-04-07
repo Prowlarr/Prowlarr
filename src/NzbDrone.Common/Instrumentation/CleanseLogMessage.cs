cd0b205a59c29b98a4b656120c9e1e82b44aff3d
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-04-07 14:10:56 -0500

    Fixed: (CinemaZ and ExoticaZ) Better Log Cleansing

--- src/NzbDrone.Common/Instrumentation/CleanseLogMessage.cs
+++ src/NzbDrone.Common/Instrumentation/CleanseLogMessage.cs
@@ -51,7 +51,7 @@ namespace NzbDrone.Common.Instrumentation
                 new Regex(@"(?<=\?|&)(X-Plex-Client-Identifier|X-Plex-Token)=(?<secret>[^&=]+?)(?= |&|$)", RegexOptions.Compiled | RegexOptions.IgnoreCase),
 
                 // Indexer Responses
-                new Regex(@"avistaz\.[a-z]{2,3}\\\/rss\\\/download\\\/(?<secret>[^&=]+?)\\\/(?<secret>[^&=]+?)\.torrent", RegexOptions.Compiled | RegexOptions.IgnoreCase),
+                new Regex(@"(?:avistaz|exoticaz|cinemaz|privatehd)\.[a-z]{2,3}\\\/rss\\\/download\\\/(?<secret>[^&=]+?)\\\/(?<secret>[^&=]+?)\.torrent", RegexOptions.Compiled | RegexOptions.IgnoreCase),
                 new Regex(@",""info_hash"":""(?<secret>[^&=]+?)"",", RegexOptions.Compiled | RegexOptions.IgnoreCase),
                 new Regex(@",""pass[- _]?key"":""(?<secret>[^&=]+?)"",", RegexOptions.Compiled | RegexOptions.IgnoreCase),
                 new Regex(@",""rss[- _]?key"":""(?<secret>[^&=]+?)"",", RegexOptions.Compiled | RegexOptions.IgnoreCase),

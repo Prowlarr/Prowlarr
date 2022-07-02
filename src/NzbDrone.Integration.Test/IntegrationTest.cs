mathispesch/Radarr

First bash at running integration tests on postgres

src/NzbDrone.Integration.Test/IntegrationTest.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
 using NLog;
+using Npgsql;
 using NUnit.Framework;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Indexers.Newznab;
 using NzbDrone.Test.Common;
 using Radarr.Http.ClientSchema;
@@ -19,6 +23,8 @@ namespace NzbDrone.Integration.Test
 
         protected int Port { get; private set; }
 
+        protected PostgresOptions PostgresOptions { get; set; } = new ();
+
         protected override string RootUrl => $"http://localhost:{Port}/";
 
         protected override string ApiKey => _runner.ApiKey;
@@ -27,7 +33,21 @@ namespace NzbDrone.Integration.Test
         {
             Port = Interlocked.Increment(ref StaticPort);
 
-            _runner = new NzbDroneRunner(LogManager.GetCurrentClassLogger(), Port);
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables("Radarr__")
+                .Build();
+
+            config.GetSection("Postgres").Bind(PostgresOptions);
+
+            if (PostgresOptions.Host != null)
+            {
+                var uid = TestBase.GetUID();
+                PostgresOptions.MainDb = uid + "_main";
+                PostgresOptions.LogDb = uid + "_log";
+                CreatePostgresDb(PostgresOptions);
+            }
+
+            _runner = new NzbDroneRunner(LogManager.GetCurrentClassLogger(), PostgresOptions, Port);
             _runner.Kill();
 
             _runner.Start();
@@ -59,6 +79,57 @@ namespace NzbDrone.Integration.Test
         protected override void StopTestTarget()
         {
             _runner.Kill();
+            if (PostgresOptions.Host != null)
+            {
+                DropPostgresDb(PostgresOptions);
+            }
+        }
+
+        private void CreatePostgresDb(PostgresOptions options)
+        {
+            var connectionString = GetConnectionString(options);
+            CreateDatabase(connectionString, options.User, options.MainDb);
+            CreateDatabase(connectionString, options.User, options.LogDb);
+        }
+
+        private void CreateDatabase(string connectionString, string user, string db)
+        {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = $"CREATE DATABASE \"{db}\" WITH OWNER = {user} ENCODING = 'UTF8' CONNECTION LIMIT = -1;";
+            cmd.ExecuteNonQuery();
+        }
+
+        private void DropPostgresDb(PostgresOptions options)
+        {
+            var connectionString = GetConnectionString(options);
+            DropDatabase(connectionString, options.MainDb);
+            DropDatabase(connectionString, options.LogDb);
+        }
+
+        private void DropDatabase(string connectionString, string db)
+        {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = $"DROP DATABASE \"{db}\";";
+            cmd.ExecuteNonQuery();
+        }
+
+        private string GetConnectionString(PostgresOptions options)
+        {
+            var builder = new NpgsqlConnectionStringBuilder()
+            {
+                Host = options.Host,
+                Port = options.Port,
+                Username = options.User,
+                Password = options.Password
+            };
+
+            return builder.ConnectionString;
         }
     }
 }

src/NzbDrone.Test.Common/NzbDroneRunner.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,7 +10,9 @@ using NUnit.Framework;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Processes;
+using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Datastore;
 using RestSharp;
 
 namespace NzbDrone.Test.Common
@@ -22,13 +25,15 @@ namespace NzbDrone.Test.Common
 
         public string AppData { get; private set; }
         public string ApiKey { get; private set; }
+        public PostgresOptions PostgresOptions { get; private set; }
         public int Port { get; private set; }
 
-        public NzbDroneRunner(Logger logger, int port = 7878)
+        public NzbDroneRunner(Logger logger, PostgresOptions postgresOptions, int port = 7878)
         {
             _processProvider = new ProcessProvider(logger);
             _restClient = new RestClient($"http://localhost:{port}/api/v3");
 
+            PostgresOptions = postgresOptions;
             Port = port;
         }
 
@@ -132,9 +137,23 @@ namespace NzbDrone.Test.Common
 
         private void Start(string outputRadarrConsoleExe)
         {
+            StringDictionary envVars = new ();
+            if (PostgresOptions?.Host != null)
+            {
+                envVars.Add("Radarr__Postgres__Host", PostgresOptions.Host);
+                envVars.Add("Radarr__Postgres__Port", PostgresOptions.Port.ToString());
+                envVars.Add("Radarr__Postgres__User", PostgresOptions.User);
+                envVars.Add("Radarr__Postgres__Password", PostgresOptions.Password);
+                envVars.Add("Radarr__Postgres__MainDb", PostgresOptions.MainDb);
+                envVars.Add("Radarr__Postgres__LogDb", PostgresOptions.LogDb);
+
+                TestContext.Progress.WriteLine("Using env vars:\n{0}", envVars.ToJson());
+            }
+
             TestContext.Progress.WriteLine("Starting instance from {0} on port {1}", outputRadarrConsoleExe, Port);
+
             var args = "-nobrowser -nosingleinstancecheck -data=\"" + AppData + "\"";
-            _nzbDroneProcess = _processProvider.Start(outputRadarrConsoleExe, args, null, OnOutputDataReceived, OnOutputDataReceived);
+            _nzbDroneProcess = _processProvider.Start(outputRadarrConsoleExe, args, envVars, OnOutputDataReceived, OnOutputDataReceived);
         }
 
         private void OnOutputDataReceived(string data)

Prowlarr/Prowlarr

(cherry picked from commit f950e80c7e4f9b088ec6a149386160eab83b61c3)

src/NzbDrone.Integration.Test/IntegrationTest.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
 using NLog;
+using Npgsql;
 using NUnit.Framework;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Datastore;
 using NzbDrone.Core.Indexers.FileList;
 using NzbDrone.Test.Common;
 using Prowlarr.Http.ClientSchema;
@@ -19,6 +23,8 @@ namespace NzbDrone.Integration.Test
 
         protected int Port { get; private set; }
 
+        protected PostgresOptions PostgresOptions { get; set; } = new ();
+
         protected override string RootUrl => $"http://localhost:{Port}/";
 
         protected override string ApiKey => _runner.ApiKey;
@@ -27,7 +33,21 @@ namespace NzbDrone.Integration.Test
         {
             Port = Interlocked.Increment(ref StaticPort);
 
-            _runner = new NzbDroneRunner(LogManager.GetCurrentClassLogger(), Port);
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables("Prowlarr__")
+                .Build();
+
+            config.GetSection("Postgres").Bind(PostgresOptions);
+
+            if (PostgresOptions.Host != null)
+            {
+                var uid = TestBase.GetUID();
+                PostgresOptions.MainDb = uid + "_main";
+                PostgresOptions.LogDb = uid + "_log";
+                CreatePostgresDb(PostgresOptions);
+            }
+
+            _runner = new NzbDroneRunner(LogManager.GetCurrentClassLogger(), PostgresOptions, Port);
             _runner.Kill();
 
             _runner.Start();
@@ -56,6 +76,57 @@ namespace NzbDrone.Integration.Test
         protected override void StopTestTarget()
         {
             _runner.Kill();
+            if (PostgresOptions.Host != null)
+            {
+                DropPostgresDb(PostgresOptions);
+            }
+        }
+
+        private void CreatePostgresDb(PostgresOptions options)
+        {
+            var connectionString = GetConnectionString(options);
+            CreateDatabase(connectionString, options.User, options.MainDb);
+            CreateDatabase(connectionString, options.User, options.LogDb);
+        }
+
+        private void CreateDatabase(string connectionString, string user, string db)
+        {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = $"CREATE DATABASE \"{db}\" WITH OWNER = {user} ENCODING = 'UTF8' CONNECTION LIMIT = -1;";
+            cmd.ExecuteNonQuery();
+        }
+
+        private void DropPostgresDb(PostgresOptions options)
+        {
+            var connectionString = GetConnectionString(options);
+            DropDatabase(connectionString, options.MainDb);
+            DropDatabase(connectionString, options.LogDb);
+        }
+
+        private void DropDatabase(string connectionString, string db)
+        {
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = $"DROP DATABASE \"{db}\";";
+            cmd.ExecuteNonQuery();
+        }
+
+        private string GetConnectionString(PostgresOptions options)
+        {
+            var builder = new NpgsqlConnectionStringBuilder()
+            {
+                Host = options.Host,
+                Port = options.Port,
+                Username = options.User,
+                Password = options.Password
+            };
+
+            return builder.ConnectionString;
         }
     }
 }

src/NzbDrone.Test.Common/NzbDroneRunner.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,7 +10,9 @@ using NUnit.Framework;
 using NzbDrone.Common.EnvironmentInfo;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Processes;
+using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Datastore;
 using RestSharp;
 
 namespace NzbDrone.Test.Common
@@ -22,13 +25,15 @@ namespace NzbDrone.Test.Common
 
         public string AppData { get; private set; }
         public string ApiKey { get; private set; }
+        public PostgresOptions PostgresOptions { get; private set; }
         public int Port { get; private set; }
 
-        public NzbDroneRunner(Logger logger, int port = 9696)
+        public NzbDroneRunner(Logger logger, PostgresOptions postgresOptions, int port = 7878)
         {
             _processProvider = new ProcessProvider(logger);
             _restClient = new RestClient($"http://localhost:{port}/api/v1");
 
+            PostgresOptions = postgresOptions;
             Port = port;
         }
 
@@ -133,9 +138,23 @@ namespace NzbDrone.Test.Common
 
         private void Start(string outputProwlarrConsoleExe)
         {
+            StringDictionary envVars = new ();
+            if (PostgresOptions?.Host != null)
+            {
+                envVars.Add("Prowlarr__Postgres__Host", PostgresOptions.Host);
+                envVars.Add("Prowlarr__Postgres__Port", PostgresOptions.Port.ToString());
+                envVars.Add("Prowlarr__Postgres__User", PostgresOptions.User);
+                envVars.Add("Prowlarr__Postgres__Password", PostgresOptions.Password);
+                envVars.Add("Prowlarr__Postgres__MainDb", PostgresOptions.MainDb);
+                envVars.Add("Prowlarr__Postgres__LogDb", PostgresOptions.LogDb);
+
+                TestContext.Progress.WriteLine("Using env vars:\n{0}", envVars.ToJson());
+            }
+
             TestContext.Progress.WriteLine("Starting instance from {0} on port {1}", outputProwlarrConsoleExe, Port);
+
             var args = "-nobrowser -nosingleinstancecheck -data=\"" + AppData + "\"";
-            _nzbDroneProcess = _processProvider.Start(outputProwlarrConsoleExe, args, null, OnOutputDataReceived, OnOutputDataReceived);
+            _nzbDroneProcess = _processProvider.Start(outputProwlarrConsoleExe, args, envVars, OnOutputDataReceived, OnOutputDataReceived);
         }
 
         private void OnOutputDataReceived(string data)
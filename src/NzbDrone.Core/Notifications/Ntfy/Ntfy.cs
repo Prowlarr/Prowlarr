4c3484a8985b315d7b2422fabb856a22089e12ee
Qstick <qstick@gmail.com>
2023-01-21 12:14:21 -0600

    New: (Notifications) Add Ntfy

---
+++ src/NzbDrone.Core/Notifications/Ntfy/Ntfy.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+
+namespace NzbDrone.Core.Notifications.Ntfy
+{
+    public class Ntfy : NotificationBase<NtfySettings>
+    {
+        private readonly INtfyProxy _proxy;
+
+        public Ntfy(INtfyProxy proxy)
+        {
+            _proxy = proxy;
+        }
+
+        public override string Name => "ntfy.sh";
+
+        public override string Link => "https://ntfy.sh/";
+
+        public override void OnHealthIssue(HealthCheck.HealthCheck message)
+        {
+            _proxy.SendNotification(HEALTH_ISSUE_TITLE_BRANDED, message.Message, Settings);
+        }
+
+        public override void OnApplicationUpdate(ApplicationUpdateMessage updateMessage)
+        {
+            _proxy.SendNotification(APPLICATION_UPDATE_TITLE_BRANDED, updateMessage.Message, Settings);
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_proxy.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}

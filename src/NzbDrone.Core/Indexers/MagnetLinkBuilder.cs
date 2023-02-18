85f8e0c451188dda480563894d88b1ce5e1711ab
Qstick <qstick@gmail.com>
2023-02-18 11:49:34 -0600

    Update MagnetLinkBuilder public trackers
    https://github.com/ngosang/trackerslist/commit/8ba874e69da0532166644922ea207da3084dff66

--- src/NzbDrone.Core/Indexers/MagnetLinkBuilder.cs
+++ src/NzbDrone.Core/Indexers/MagnetLinkBuilder.cs
@@ -11,25 +11,25 @@ namespace NzbDrone.Core.Indexers
         private static readonly List<string> _trackers = new List<string>
         {
             "udp://tracker.opentrackr.org:1337/announce",
-            "http://p4p.arenabg.com:1337/announce",
             "udp://9.rarbg.com:2810/announce",
             "udp://tracker.openbittorrent.com:6969/announce",
-            "udp://www.torrent.eu.org:451/announce",
-            "udp://tracker.torrent.eu.org:451/announce",
-            "udp://retracker.lanta-net.ru:2710/announce",
-            "udp://open.stealth.si:80/announce",
-            "udp://exodus.desync.com:6969/announce",
-            "http://openbittorrent.com:80/announce",
+            "http://tracker.openbittorrent.com:80/announce",
             "udp://opentracker.i2p.rocks:6969/announce",
-            "udp://opentor.org:2710/announce",
-            "udp://ipv4.tracker.harry.lu:80/announce",
-            "udp://tracker.uw0.xyz:6969/announce",
+            "https://opentracker.i2p.rocks:443/announce",
+            "udp://www.peckservers.com:9000/announce",
+            "udp://tracker.torrent.eu.org:451/announce",
+            "udp://tracker.tiny-vps.com:6969/announce",
             "udp://tracker.moeking.me:6969/announce",
             "udp://tracker.dler.org:6969/announce",
+            "udp://tracker.altrosky.nl:6969/announce",
+            "udp://p4p.arenabg.com:1337/announce",
+            "udp://open.stealth.si:80/announce",
+            "udp://open.demonii.com:1337/announce",
+            "udp://ipv4.tracker.harry.lu:80/announce",
             "udp://explodie.org:6969/announce",
-            "udp://bt2.archive.org:6969/announce",
+            "udp://exodus.desync.com:6969/announce",
             "udp://bt1.archive.org:6969/announce",
-            "https://trakx.herokuapp.com:443/announce",
+            "https://tracker.lilithraws.org:443/announce"
         };
 
         public static string BuildPublicMagnetLink(string infoHash, string releaseTitle)

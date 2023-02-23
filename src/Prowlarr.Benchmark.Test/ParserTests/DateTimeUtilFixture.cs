495f61f4125fddc0642364661f8f130913c48a47
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-02-23 19:29:13 +0200

    Improve unix timestamp performance

--- src/Prowlarr.Benchmark.Test/ParserTests/DateTimeUtilFixture.cs
+++ src/Prowlarr.Benchmark.Test/ParserTests/DateTimeUtilFixture.cs
@@ -24,6 +24,7 @@ namespace NzbDrone.Benchmark.Test.ParserTests
 
         [Benchmark]
         [Arguments("2022-08-08 02:07:39")]
+        [Arguments("1677173189")]
         public void parse_unknown_date(string dateInput)
         {
             DateTimeUtil.FromUnknown(dateInput);
@@ -31,6 +32,7 @@ namespace NzbDrone.Benchmark.Test.ParserTests
 
         [Benchmark]
         [Arguments("2022-08-08 02:07:39 -02:00", "2006-01-02 15:04:05 -07:00")]
+        [Arguments("2022-08-08 02:07:39 -02:00", "yyyy-MM-dd HH:mm:ss zzz")]
         public void parse_datetime_golang(string dateInput, string format)
         {
             DateTimeUtil.ParseDateTimeGoLang(dateInput, format);

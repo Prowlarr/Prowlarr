8d39a7f315687f91950e63c60b61a67819089283
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-03-16 02:53:34 +0200

    Fixed: (AnimeTorrents) Set `SupportsPagination`

--- src/NzbDrone.Core/Indexers/Definitions/AnimeTorrents.cs
+++ src/NzbDrone.Core/Indexers/Definitions/AnimeTorrents.cs
@@ -26,6 +26,7 @@ namespace NzbDrone.Core.Indexers.Definitions
         public override string Description => "Definitive source for anime and manga";
         public override DownloadProtocol Protocol => DownloadProtocol.Torrent;
         public override IndexerPrivacy Privacy => IndexerPrivacy.Private;
+        public override bool SupportsPagination => true;
         public override TimeSpan RateLimit => TimeSpan.FromSeconds(4);
         public override IndexerCapabilities Capabilities => SetCapabilities();
 

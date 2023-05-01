fcd2b1ffae99a4b88a2d546da4aa662250afd852
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-05-01 12:52:00 +0300

    Check response status code in classes that extend RssParser

--- src/NzbDrone.Core/Indexers/Definitions/Torznab/TorznabRssParser.cs
+++ src/NzbDrone.Core/Indexers/Definitions/Torznab/TorznabRssParser.cs
@@ -28,6 +28,11 @@ namespace NzbDrone.Core.Indexers.Torznab
 
         protected override bool PreProcess(IndexerResponse indexerResponse)
         {
+            if (indexerResponse.HttpResponse.HasHttpError)
+            {
+                base.PreProcess(indexerResponse);
+            }
+
             var xdoc = LoadXmlDocument(indexerResponse);
             var error = xdoc.Descendants("error").FirstOrDefault();
 

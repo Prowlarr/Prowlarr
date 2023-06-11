7b551a0af1d23b7fcee42ee278a184d5620c36ab
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-06-11 07:42:41 +0300

    Update Anidub description

--- src/NzbDrone.Core/Indexers/Definitions/Anidub.cs
+++ src/NzbDrone.Core/Indexers/Definitions/Anidub.cs
@@ -25,7 +25,7 @@ namespace NzbDrone.Core.Indexers.Definitions
     {
         public override string Name => "Anidub";
         public override string[] IndexerUrls => new[] { "https://tr.anidub.com/" };
-        public override string Description => "Anidub is russian anime voiceover group and eponymous anime tracker.";
+        public override string Description => "Anidub is RUSSIAN anime voiceover group and eponymous anime tracker.";
         public override string Language => "ru-RU";
         public override Encoding Encoding => Encoding.UTF8;
         public override DownloadProtocol Protocol => DownloadProtocol.Torrent;

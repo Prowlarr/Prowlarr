e2e52746bb78f9463573c216c756fdab8aae3be1
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-07-07 17:26:56 +0300

    Fix repeat search when limits are empty

--- frontend/src/History/HistoryRowConnector.js
+++ frontend/src/History/HistoryRowConnector.js
@@ -1,4 +1,5 @@
 import { push } from 'connected-react-router';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
@@ -49,14 +50,14 @@ class HistoryRowConnector extends Component {
   // Listeners
 
   onSearchPress = (query, indexerId, categories, type, limit, offset) => {
-    this.props.setSearchDefault({
+    this.props.setSearchDefault(_.pickBy({
       searchQuery: query,
       searchIndexerIds: [indexerId],
       searchCategories: categories,
       searchType: type,
       searchLimit: limit,
       searchOffset: offset
-    });
+    }));
     this.props.push(`${window.Prowlarr.urlBase}/search`);
   };
 

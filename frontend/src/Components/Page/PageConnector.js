ad85f66450f4a08899ceee32b2d0b2d96cdf6b74
Mark McDowall <mark@mcdowall.ca>
2023-07-14 16:55:25 -0700

    Fixed: Ensure translations are fetched before loading app
    (cherry picked from commit ad2721dc55f3233e4c299babe5744418bc530418)

--- frontend/src/Components/Page/PageConnector.js
+++ frontend/src/Components/Page/PageConnector.js
@@ -232,6 +232,7 @@ class PageConnector extends Component {
 
   render() {
     const {
+      hasTranslationsError,
       isPopulated,
       hasError,
       dispatchFetchTags,
@@ -245,11 +246,12 @@ class PageConnector extends Component {
       ...otherProps
     } = this.props;
 
-    if (hasError || !this.state.isLocalStorageSupported) {
+    if (hasTranslationsError || hasError || !this.state.isLocalStorageSupported) {
       return (
         <ErrorPage
           {...this.state}
           {...otherProps}
+          hasTranslationsError={hasTranslationsError}
         />
       );
     }
@@ -270,6 +272,7 @@ class PageConnector extends Component {
 }
 
 PageConnector.propTypes = {
+  hasTranslationsError: PropTypes.bool.isRequired,
   isPopulated: PropTypes.bool.isRequired,
   hasError: PropTypes.bool.isRequired,
   isSidebarVisible: PropTypes.bool.isRequired,

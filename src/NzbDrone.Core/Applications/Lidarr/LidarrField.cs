e0dddfa215a68a15473ab3301077931ff07537ea
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-08-08 18:30:25 +0300

    Remove Order and Help columns from Apps Fields

--- src/NzbDrone.Core/Applications/Lidarr/LidarrField.cs
+++ src/NzbDrone.Core/Applications/Lidarr/LidarrField.cs
@@ -2,12 +2,9 @@ namespace NzbDrone.Core.Applications.Lidarr
 {
     public class LidarrField
     {
-        public int Order { get; set; }
         public string Name { get; set; }
         public string Label { get; set; }
         public string Unit { get; set; }
-        public string HelpText { get; set; }
-        public string HelpLink { get; set; }
         public object Value { get; set; }
         public string Type { get; set; }
         public bool Advanced { get; set; }

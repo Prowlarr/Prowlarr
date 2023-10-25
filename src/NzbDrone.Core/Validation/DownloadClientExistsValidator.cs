6169fc2fa3c063887679857ab1b4b5d038771a9d
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-10-25 15:11:41 +0300

    New: Add Download Client validation for indexers

---
+++ src/NzbDrone.Core/Validation/DownloadClientExistsValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Validators;
+using NzbDrone.Core.Download;
+
+namespace NzbDrone.Core.Validation
+{
+    public class DownloadClientExistsValidator : PropertyValidator
+    {
+        private readonly IDownloadClientFactory _downloadClientFactory;
+
+        public DownloadClientExistsValidator(IDownloadClientFactory downloadClientFactory)
+        {
+            _downloadClientFactory = downloadClientFactory;
+        }
+
+        protected override string GetDefaultMessageTemplate() => "Download Client does not exist";
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            return context?.PropertyValue == null || _downloadClientFactory.Exists((int)context.PropertyValue);
+        }
+    }
+}
--- src/Prowlarr.Api.V1/Indexers/IndexerController.cs
+++ src/Prowlarr.Api.V1/Indexers/IndexerController.cs
@@ -7,12 +7,18 @@ namespace Prowlarr.Api.V1.Indexers
     [V1ApiController]
     public class IndexerController : ProviderControllerBase<IndexerResource, IndexerBulkResource, IIndexer, IndexerDefinition>
     {
-        public IndexerController(IndexerFactory indexerFactory, IndexerResourceMapper resourceMapper, IndexerBulkResourceMapper bulkResourceMapper, AppProfileExistsValidator appProfileExistsValidator)
+        public IndexerController(IndexerFactory indexerFactory,
+            IndexerResourceMapper resourceMapper,
+            IndexerBulkResourceMapper bulkResourceMapper,
+            AppProfileExistsValidator appProfileExistsValidator,
+            DownloadClientExistsValidator downloadClientExistsValidator)
             : base(indexerFactory, "indexer", resourceMapper, bulkResourceMapper)
         {
             Http.Validation.RuleBuilderExtensions.ValidId(SharedValidator.RuleFor(s => s.AppProfileId));
+            Http.Validation.RuleBuilderExtensions.ValidId(SharedValidator.RuleFor(s => s.DownloadClientId));
 
             SharedValidator.RuleFor(c => c.AppProfileId).SetValidator(appProfileExistsValidator);
+            SharedValidator.RuleFor(c => c.DownloadClientId).SetValidator(downloadClientExistsValidator);
         }
     }
 }

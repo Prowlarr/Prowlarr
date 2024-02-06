4ede5dced026c9c33f8454a8f6aca0bbbc85abd3
abcasada <40399292+abcasada@users.noreply.github.com>
2024-02-06 13:51:22 -0600

    Add hints to date examples
    Leave day in example date greater than 12 to not confuse with a month; add hints to indicate the difference between D and DD
    
    Co-authored-by: Bogdan <mynameisbogdan@users.noreply.github.com>

--- frontend/src/Settings/UI/UISettings.js
+++ frontend/src/Settings/UI/UISettings.js
@@ -28,12 +28,12 @@ export const weekColumnOptions = [
 ];
 
 const shortDateFormatOptions = [
-  { key: 'MMM D YYYY', value: 'Mar 5 2014' },
-  { key: 'DD MMM YYYY', value: '05 Mar 2014' },
-  { key: 'MM/D/YYYY', value: '03/5/2014' },
-  { key: 'MM/DD/YYYY', value: '03/05/2014' },
-  { key: 'DD/MM/YYYY', value: '05/03/2014' },
-  { key: 'YYYY-MM-DD', value: '2014-03-05' }
+  { key: 'MMM D YYYY', value: 'Mar 25 2014', hint: 'MMM D YYYY' },
+  { key: 'DD MMM YYYY', value: '25 Mar 2014', hint: 'DD MMM YYYY' },
+  { key: 'MM/D/YYYY', value: '03/25/2014', hint: 'MM/D/YYYY' },
+  { key: 'MM/DD/YYYY', value: '03/25/2014', hint: 'MM/DD/YYYY' },
+  { key: 'DD/MM/YYYY', value: '25/03/2014', hint: 'DD/MM/YYYY' },
+  { key: 'YYYY-MM-DD', value: '2014-03-25', hint: 'YYYY-MM-DD' }
 ];
 
 const longDateFormatOptions = [

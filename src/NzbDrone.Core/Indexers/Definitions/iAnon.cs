2aa57dfaed314a1c873cba1780d6fe842dfeb4b9
martadams89 <martadams@hotmail.com>
2024-03-15 07:44:00 +0000

    feat: add ianon indexer

---
+++ src/NzbDrone.Core/Indexers/Definitions/iAnon.cs
@@ -0,0 +1,39 @@
+using NLog;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Indexers.Definitions.Gazelle;
+using NzbDrone.Core.Messaging.Events;
+
+namespace NzbDrone.Core.Indexers.Definitions;
+
+public class BrokenStones : GazelleBase<GazelleSettings>
+{
+    public override string Name => "iAnon";
+    public override string[] IndexerUrls => new[] { "https://iAnon.app/" };
+    public override string Description => "iAnon is a Private site for MacOS and iOS APPS / GAMES";
+    public override IndexerPrivacy Privacy => IndexerPrivacy.Private;
+
+    public BrokenStones(IIndexerHttpClient httpClient,
+                        IEventAggregator eventAggregator,
+                        IIndexerStatusService indexerStatusService,
+                        IConfigService configService,
+                        Logger logger)
+        : base(httpClient, eventAggregator, indexerStatusService, configService, logger)
+    {
+    }
+
+    protected override IndexerCapabilities SetCapabilities()
+    {
+        var caps = new IndexerCapabilities();
+
+        caps.Categories.AddCategoryMapping(1, NewznabStandardCategory.PCMac, "MacOS Apps");
+        caps.Categories.AddCategoryMapping(2, NewznabStandardCategory.PCMac, "MacOS Games");
+        caps.Categories.AddCategoryMapping(3, NewznabStandardCategory.PCMobileiOS, "iOS Apps");
+        caps.Categories.AddCategoryMapping(4, NewznabStandardCategory.PCMobileiOS, "iOS Games");
+        caps.Categories.AddCategoryMapping(5, NewznabStandardCategory.Other, "Graphics");
+        caps.Categories.AddCategoryMapping(6, NewznabStandardCategory.Audio, "Audio");
+        caps.Categories.AddCategoryMapping(7, NewznabStandardCategory.Other, "Tutorials");
+        caps.Categories.AddCategoryMapping(8, NewznabStandardCategory.Other, "Other");
+
+        return caps;
+    }
+}

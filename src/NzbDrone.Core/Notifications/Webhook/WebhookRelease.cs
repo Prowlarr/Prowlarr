5bc5f0e6b8218b6dd3da2ddb342e8a5e7370a52e
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-03-25 12:57:39 +0200

    New: Categories, genres, indexer flags and publish dates for webhook releases

--- src/NzbDrone.Core/Notifications/Webhook/WebhookRelease.cs
+++ src/NzbDrone.Core/Notifications/Webhook/WebhookRelease.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Notifications.Webhook
@@ -13,10 +16,18 @@ namespace NzbDrone.Core.Notifications.Webhook
             ReleaseTitle = release.Title;
             Indexer = release.Indexer;
             Size = release.Size;
+            Categories = release.Categories.Select(f => f.Name).ToList();
+            Genres = release.Genres.ToList();
+            IndexerFlags = release.IndexerFlags.Select(f => f.Name).ToHashSet();
+            PublishDate = release.PublishDate;
         }
 
         public string ReleaseTitle { get; set; }
         public string Indexer { get; set; }
         public long? Size { get; set; }
+        public List<string> Categories { get; set; }
+        public List<string> Genres { get; set; }
+        public HashSet<string> IndexerFlags { get; set; }
+        public DateTime? PublishDate { get; set; }
     }
 }

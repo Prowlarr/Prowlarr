f56890687668a958554d066e93d93353e05ab383
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-09-06 20:15:55 +0300

    Bump FluentMigrator to official 6.2.0

--- src/NzbDrone.Core/Datastore/Migration/Framework/MigrationExtension.cs
+++ src/NzbDrone.Core/Datastore/Migration/Framework/MigrationExtension.cs
@@ -1,11 +1,17 @@
-﻿using FluentMigrator;
+using System.Data;
+using FluentMigrator;
 using FluentMigrator.Builders.Create;
 using FluentMigrator.Builders.Create.Table;
 using FluentMigrator.Runner;
 using FluentMigrator.Runner.BatchParser;
+using FluentMigrator.Runner.Generators;
 using FluentMigrator.Runner.Generators.SQLite;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors;
 using FluentMigrator.Runner.Processors.SQLite;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace NzbDrone.Core.Datastore.Migration.Framework
 {
@@ -16,23 +22,49 @@ namespace NzbDrone.Core.Datastore.Migration.Framework
             return expressionRoot.Table(name).WithColumn("Id").AsInt32().PrimaryKey().Identity();
         }
 
-        public static void AddParameter(this System.Data.IDbCommand command, object value)
+        public static IDbCommand CreateCommand(this IDbConnection conn, IDbTransaction tran, string query)
+        {
+            var command = conn.CreateCommand();
+            command.Transaction = tran;
+            command.CommandText = query;
+
+            return command;
+        }
+
+        public static void AddParameter(this IDbCommand command, object value)
         {
             var parameter = command.CreateParameter();
             parameter.Value = value;
             command.Parameters.Add(parameter);
         }
 
-        public static IMigrationRunnerBuilder AddNzbDroneSQLite(this IMigrationRunnerBuilder builder)
+        public static IMigrationRunnerBuilder AddNzbDroneSQLite(this IMigrationRunnerBuilder builder, bool binaryGuid = false, bool useStrictTables = false)
         {
             builder.Services
                 .AddTransient<SQLiteBatchParser>()
                 .AddScoped<SQLiteDbFactory>()
-                .AddScoped<NzbDroneSQLiteProcessor>()
+                .AddScoped<NzbDroneSQLiteProcessor>(sp =>
+                {
+                    var factory = sp.GetService<SQLiteDbFactory>();
+                    var logger = sp.GetService<ILogger<NzbDroneSQLiteProcessor>>();
+                    var options = sp.GetService<IOptionsSnapshot<ProcessorOptions>>();
+                    var connectionStringAccessor = sp.GetService<IConnectionStringAccessor>();
+                    var sqliteQuoter = new SQLiteQuoter(false);
+                    return new NzbDroneSQLiteProcessor(factory, sp.GetService<SQLiteGenerator>(), logger, options, connectionStringAccessor, sp, sqliteQuoter);
+                })
+                .AddScoped<ISQLiteTypeMap>(_ => new NzbDroneSQLiteTypeMap(useStrictTables))
                 .AddScoped<IMigrationProcessor>(sp => sp.GetRequiredService<NzbDroneSQLiteProcessor>())
-                .AddScoped<SQLiteQuoter>()
-                .AddScoped<SQLiteGenerator>()
+                .AddScoped(
+                    sp =>
+                    {
+                        var typeMap = sp.GetRequiredService<ISQLiteTypeMap>();
+                        return new SQLiteGenerator(
+                            new SQLiteQuoter(binaryGuid),
+                            typeMap,
+                            new OptionsWrapper<GeneratorOptions>(new GeneratorOptions()));
+                    })
                 .AddScoped<IMigrationGenerator>(sp => sp.GetRequiredService<SQLiteGenerator>());
+
             return builder;
         }
     }

baee6a6d33700e33c1574fadf77ca96c894fbd12
Leandro Battochio <lbattochio@hotmail.com>
2026-04-09 23:25:23 -0300

    Add missing BjShare Linq import

--- src/NzbDrone.Core/Indexers/Definitions/BjShare.cs
+++ src/NzbDrone.Core/Indexers/Definitions/BjShare.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using AngleSharp.Dom;
 using AngleSharp.Html.Parser;

Sonarr/Sonarr

Don't skip health check if Allowed Hosts is set to *

src/NzbDrone.Core.Test/HealthCheck/Checks/AllowedHostsCheckFixture.cs
@@ -62,6 +62,19 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
             Subject.Check().ShouldBeOk();
         }
 
+        [TestCase("*")]
+        [TestCase("*, sonarr.local")]
+        [TestCase("sonarr.local; *")]
+        public void should_return_warning_when_allowed_hosts_contains_wildcard(string allowedHosts)
+        {
+            GivenAllowedHosts(allowedHosts);
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeWarning();
+
+            ExceptionVerification.ExpectedWarns(1);
+        }
+
         [Test]
         public void should_return_warning_when_allowed_hosts_is_whitespace()
         {

Prowlarr/Prowlarr

(cherry picked from commit 27aac304eebb2dbf550ab40ae0d5c5cee9322562)

src/NzbDrone.Core.Test/HealthCheck/Checks/AllowedHostsCheckFixture.cs
@@ -62,6 +62,19 @@ namespace NzbDrone.Core.Test.HealthCheck.Checks
             Subject.Check().ShouldBeOk();
         }
 
+        [TestCase("*")]
+        [TestCase("*, prowlarr.local")]
+        [TestCase("prowlarr.local; *")]
+        public void should_return_warning_when_allowed_hosts_contains_wildcard(string allowedHosts)
+        {
+            GivenAllowedHosts(allowedHosts);
+            GivenAuthenticationRequired(AuthenticationRequiredType.DisabledForLocalAddresses);
+
+            Subject.Check().ShouldBeWarning();
+
+            ExceptionVerification.ExpectedWarns(1);
+        }
+
         [Test]
         public void should_return_warning_when_allowed_hosts_is_whitespace()
         {